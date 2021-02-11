9f52972f1d7d7f9ceb81dcf1b8c6fe0d7ce0f915
Your Name <you@example.com>
2021-02-11 09:16:38 +0200

    t3

---
+++ tasks/mongoose Task/src/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = new mongoose.model('User',{
+    name:{
+        type: String, 
+        required:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+       type:String,
+       trim:true,
+       required:true,
+       validate(value){
+           if(!validator.isEmail(value)){
+               throw new Error('email is invalid')
+           }
+       }
+    },
+    phone:{
+        type:String,
+        trim:true,
+        validate(value){
+            if(!validator.isMobilePhone(value,'ar-EG')){
+                throw new Error('phone is invalid')
+            }
+        }
+     }
+})
+
+module.exports=User
